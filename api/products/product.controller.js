625bce58d15cc5fb6cfd087943b527a07eeb7acb
oingbong <oingbong@gmail.com>
2017-06-04 21:27:33 +0900

    Connect DB & controller

--- api/products/product.controller.js
+++ api/products/product.controller.js
@@ -1,52 +1,40 @@
-
-
-// 임시 데이터
-let products = [
-	{
-		id : 1,
-		name : 'tera'
-	},
-	{
-		id : 2,
-		name : '8percent'
-	},
-	{
-		id : 3,
-		name : 'villy'
-	}	
-];
+const models = require('../../models');
 
 exports.index = (req, res) => {
-	return res.json(products);
+	models.Product.findAll()
+		.then(products => res.json(products));
 };
 
 exports.show = (req, res) => {
 	const id = parseInt(req.params.id, 10);
 	if(!id){
 		return res.status(400).json({error : 'Incorrect id'});
-	}
+	};
 
-	let product = products.filter(product => product.id === id)[0];
-	if(!product){
-		return res.status(404).json({error : 'Unknown product'});
-	}
+	models.Product.findOne({
+		where : {
+			id : id
+		}
+	}).then(product => {
+		if(!product){
+			return res.status(404).json({error : 'No Product'});
+		}
 
-	return res.json(product);
+		return res.json(product);
+	});
 };
 
 exports.destroy = (req, res) => {
 	const id = parseInt(req.params.id, 10);
 	if(!id){
-		return res.status(400).json({error : 'Incorrect id'});
-	}
+		return res.status(400).json({error: 'Incorrect id'});
+	};
 
-	const productIdx = products.findIndex(product => product.id === id);
-	if(productIdx === -1){
-		return res.status(404).json({error : 'Unknown product'});
-	}
-
-	products.splice(productIdx, 1);
-	res.status(204).send();
+	models.Product.destroy({
+		where : {
+			id : id
+		}
+	}).then(() => res.status(204).send());
 };
 
 exports.create = (req, res) => {
@@ -55,17 +43,7 @@ exports.create = (req, res) => {
 		return res.status(400).json({error : 'Incorrect name'});
 	}
 
-	// 현재 데이터 기준 id 값보다 1 더 큰 id 를 만들기 위한 작업
-	const id = products.reduce((maxId, product) => {
-		return product.id > maxId ? product.id : maxId
-	}, 0) +1;
-
-	// 배열에 상품 추가
-	const newProduct = {
-		id : id,
+	models.Product.create({
 		name : name
-	};
-	products.push(newProduct);
-
-	return res.status(201).json(newProduct);
+	}).then((product) => res.status(201).json(product));
 };
