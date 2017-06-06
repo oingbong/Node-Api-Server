3866e5006960bdddf28b389bec2e1fbaf895a2c8
oingbong <oingbong@gmail.com>
2017-06-06 15:29:04 +0900

    Add Test Data in test DB

--- api/products/product.spec.js
+++ api/products/product.spec.js
@@ -1,8 +1,34 @@
 const should = require('should');
 const request = require('supertest');
 const app = require('../../app');
+const syncDatabase = require('../../bin/sync-database'); // DB sync Module
+const models = require('../../models'); // DB Connect
 
 describe('GET /products', () => {
+
+	// test DB sync
+	before('sync database', (done) => {
+		// sync data base
+		syncDatabase().then(() => {
+			done();
+		});
+	});
+
+	const products = [
+		{name : 'siso'},
+		{name : 'beyond'},
+		{name : 'people'}
+	];
+
+	/*
+		sequelize 모델에는 두종류 함수 : create(), bulkCreate()
+		create() - 하나의 로우 생성 함수
+		bulkCreate() - 여러개 데이터를 배열로 받아 여러개 로우 생성 함수
+	*/
+	before('insert 3 products into databse', (done) => {
+		models.Product.bulkCreate(products).then(() => done());
+	});
+
 	it('should return 200 status code(GET)', (done) => {
 		request(app)
 		.get('/products')
@@ -30,16 +56,16 @@ describe('GET /products', () => {
 	});
 });
 
-describe('PUT /products/:id', () => {
-	it.only('should return 200 status code(PUT)', (done) => {
-		request(app)
-			.put('/products/1')
-			.send({
-				name : 'foo'
-			})
-			.end((err, res) => {
-				if(err) throw err;
-				done();
-			});
-	});
-});
+// describe('PUT /products/:id', () => {
+// 	it.only('should return 200 status code(PUT)', (done) => {
+// 		request(app)
+// 			.put('/products/1')
+// 			.send({
+// 				name : 'foo'
+// 			})
+// 			.end((err, res) => {
+// 				if(err) throw err;
+// 				done();
+// 			});
+// 	});
+// });
